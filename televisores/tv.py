dfe05a551630b33c31bbabb3cf14ba041836d4ce
Jhorlan-Ortega <124930448+Jhorlan-Ortega@users.noreply.github.com>
2023-02-28 08:52:22 -0500

    Intento_3

--- televisores/tv.py
+++ televisores/tv.py
@@ -1,14 +1,3 @@
-class Marca:
-    def __init__(self, nombre):
-        self._nombre = nombre
-
-    def setNombre(self, nombre):
-        self._nombre = nombre
-
-    def getNombre(self):
-        return self._nombre
-
-
 class TV:
     _numTV = 0
 
@@ -95,40 +84,3 @@ class TV:
             self._volumen = nVolumen
         else:
             pass
-
-
-class Control:
-
-    def __init__(self):
-        self._tv = None
-
-    def enlazar(self, tv):
-        self._tv = tv
-        tv.setControl(self)
-
-    def turnOn(self):
-        self._tv.turnOn()
-
-    def canalUp(self):
-        self._tv.canalUp()
-
-    def volumenUp(self):
-        self._tv.volumenUp()
-
-    def turnOff(self):
-        self._tv.turnOff()
-
-    def canalDown(self):
-        self._tv.canalDown()
-
-    def volumenDown(self):
-        self._tv.volumenDown()
-
-    def setCanal(self, canal):
-        self._tv.setCanal(canal)
-
-    def getTv(self):
-        return self._tv
-
-    def setTv(self, tv):
-        self.tv = tv
